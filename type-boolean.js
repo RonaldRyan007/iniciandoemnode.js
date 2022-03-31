9779c6c3de25e0fa095492fd805845d7dbbb296d
Ronald Ryan <ronaldryan006@gmail.com>
2022-03-31 13:45:39 -0300

    primeiros estudos

---
+++ type-boolean.js
@@ -0,0 +1,9 @@
+const numero1 = 5;
+const numero2 = 5;
+
+console.log(numero1 === numero2);
+
+const primeiroTexto = "Alura";
+const segundoTexto = "alura";
+
+console.log(primeiroTexto === segundoTexto);
---
+++ type-string.js
@@ -0,0 +1,15 @@
+const cifrao = '\u0024'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao);
+console.log(tique);
+console.log(hiragana);
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinisculo = input.toLowerCase();
+
+console.log(cidade === inputMinisculo);
+
