7778ea0d9d191eff18ad9c0d30ee9ed582be7215
Ronald Ryan <ronaldryan006@gmail.com>
2022-03-31 16:43:30 -0300

    yep

--- type-number.js
+++ type-number.js
@@ -4,3 +4,8 @@ const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero + segundoNumero;
 
 console.log(operacaoMatematica);
+
+const soma = 10+10
+const multiplicacao = 10*10
+
+console.log((soma + multiplicacao) + 10);
