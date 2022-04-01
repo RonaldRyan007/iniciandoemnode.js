5b16e042f0417b960489337a2010e8004db68603
Ronald Ryan <ronaldryan006@gmail.com>
2022-04-01 12:26:44 -0300

    main

---
+++ script.js
@@ -0,0 +1,35 @@
+//var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+//let
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if(altura === 'retângulo'){
+//     area = altura * comprimento
+// }else{
+//     area = (altura * comprimento)/2
+// }
+// console.log(area);
+
+//const 
+
+const forma = 'retangulo'
+const altura = 5;
+const comprimento = 7;
+
+if(forma === 'retangulo'){
+    area = altura * comprimento
+}else{
+    altura (altura * comprimento)/2
+}
+console.log(area);
