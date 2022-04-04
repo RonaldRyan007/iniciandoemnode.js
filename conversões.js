fb8ae5df6d117ac33aab1987319911f5eb48b63e
Ronald Ryan <ronaldryan006@gmail.com>
2022-04-04 14:05:02 -0300

    conversões e bollean part2

---
+++ conversões.js
@@ -0,0 +1,9 @@
+//conversões implícitas
+
+const  numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString));
+
+//conversões explícitas
+
---
+++ truthy-falsy.js
@@ -0,0 +1,22 @@
+//bollean
+const usuarioLogado = true
+const contaPaga = false
+
+//truthy e falsy
+
+// console.log("" == false);
+// console.log(0 == false);
+
+//null e undefenid
+
+// let minhaVar;
+// let varNull = null;
+
+// console.log(minhaVar);
+// console.log(varNull);
+
+const number = 3;
+const text = "Alura";
+
+console.log(typeof number);
+console.log(typeof text);
